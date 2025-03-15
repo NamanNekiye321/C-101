44c9979ae2debb5648cabec7fde96ba74e898930
NamanNekiye321 <namannekiye321@gmail.com>
2025-03-15 21:58:33 +0530

    commit

--- test.c
+++ test.c
@@ -1,8 +1,22 @@
-#include <stdio.h>
+/*
+    Author:Naman Nekiye
+    Purpose:I am learning C Programming
+    Date: 15/03/2025
+*/
+
+#include "stdio.h"
 
 int main()
 {
-    printf("Naman\n");
-    scanf("%d");
+
+    int value1, value2, sum;
+
+    value1 = 50;
+    value2 = 25;
+    sum = value1 + value2;
+
+    printf("the sum of %i and %i is %i\n", value1, value2, sum);
+
     return 0;
 }
+
