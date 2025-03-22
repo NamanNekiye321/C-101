f9ba2f7897e45e3d12535287e090213550e0b212
NamanNekiye321 <namannekiye321@gmail.com>
2025-03-22 20:54:20 +0530

    commit

--- enumandchar.c
+++ enumandchar.c
@@ -1,70 +1,69 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main() {
-    
-    enum primaryColor {red, yellow, blue};
+int main()
+{
+
+    enum primaryColor
+    {
+        red,
+        yellow,
+        blue
+    };
     enum primaryColor myColor, gregsColor;
 
     myColor = red;
 
-    enum month {January, February, March, April, May, June, July, August, September, October, November, December};
+    enum month
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    };
     enum month myBirthdayMonth;
 
     myBirthdayMonth = August;
 
     printf("My birtday month is 11th %d\n", myBirthdayMonth);
 
-
     printf("Hello World");
 
-
-
-
-    char broiled ;
+    char broiled;
     broiled = 't';
 
     printf("value of it is %c\n", broiled);
 
-
     char asci = 66;
-    
+
     printf("value of it is %c\n", asci);
 
-
-    
-
-    enum gender {male, female};
+    enum gender
+    {
+        male,
+        female
+    };
     enum gender myGender = male;
 
-    enum gender  anotherGender = female;
+    enum gender anotherGender = female;
 
     bool isMale = (myGender == anotherGender);
 
-
     printf("myGender is equal to anotherGender: %d\n ", isMale);
-
 
     char myCharacter = '\n';
 
     printf("%c", myCharacter);
 
     printf("Hello World");
-
-
-
-
-
-    
-    
-
-
-
-
-
-
-
-
 
     return 0;
 }
